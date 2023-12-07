931f97f1ed6194672317a431dd6410aad9c2c523
FramedezA <94976703+FramedezA@users.noreply.github.com>
2023-12-07 22:16:10 +0300

    1 task

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+while True:
+    str = input("Введите строку:")
+    new_str = ''
+    flag = False
+    for ch in str:
+        if ch == ")":
+            flag  =False
+        if not flag:
+            new_str+=ch
+        if ch == "(":
+            flag = True
+    print('Новая строка: ')
+    print(new_str)
